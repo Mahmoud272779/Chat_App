0e609acd0508c8a97c70a57869f5f44a74262bff
Mahmoud El-Sayed <me974931@gmail.com>
2024-04-24 17:20:07 +0200

    first commit

---
+++ singalR_Test/singalR_Test/ChatHub.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+
+namespace singalR_Test
+{
+    [HubName("chat")]
+    public class ChatHub : Hub
+    {
+        public void sendMessage(string name , string message)
+        {
+            Clients.All.newMessage(name,message);
+        }
+    }
+}
